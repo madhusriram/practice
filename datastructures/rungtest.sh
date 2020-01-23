a031af7ed6faec54614184c1ca51a0ed034774cd
Madhukar Sriramu <madhusirmv@gmail.com>
2020-01-22 22:02:54 -0500

    install gdb in the container, have ability to attach to container

--- datastructures/rungtest.sh
+++ datastructures/rungtest.sh
@@ -1,17 +1,30 @@
 # Starting script for testing
 
 IMG_NAME=testimg
-# Build the testimg container
+
+dockershell=
+
+# Build the container
 function build() {
 	docker build . -t ${IMG_NAME}
 }
 
-if [[ "$(docker images -q testimg 2> /dev/null)" == "" ]] || [[ $1 == "-f" ]]; then
+for i in "$@"; do
+	case "$i" in
+		-f)
+			build
+			shift
+			;;
+		-d)
+			dockershell="/bin/bash"
+			shift
+			;;
+	esac
+done
+
+if [[ "$(docker images -q testimg 2> /dev/null)" == "" ]]; then
 	build
 fi
 
 # Run it now by mapping the volume of interest
-docker run --rm  -v ${PWD}:/source -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined ${IMG_NAME}
-
-# Blow up the build directory
-#rm -rf build
+docker run --rm  -v ${PWD}:/source -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined ${IMG_NAME} ${dockershell}
