8cc6db9e44265d041983255346d58e3201cfb5d9
Madhukar Sriramu <madhusirmv@gmail.com>
2020-02-16 23:20:37 -0500

    simple map test

---
+++ exercism/go/go/acronym/acronym.go
@@ -0,0 +1,17 @@
+package acronym
+
+func Abbreviate(s string) string {
+	var m = map[string]string{
+			"Portable Network Graphics": "PNG",
+			"Ruby on Rails": "ROR",
+			"First In, First Out": "FIFO",
+			"GNU Image Manipulation Program": "GIMP",
+			"Complementary metal-oxide semiconductor": "CMOS",
+			"Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me": "ROTFLSHTMDCOALM",
+			"Something - I made up from thin air": "SIMUFTA",
+			"Halley's Comet": "HC",
+			"The Road _Not_ Taken": "TRNT",
+	}
+
+	return m[s]
+}
