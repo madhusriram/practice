f654b85873a423911ce1f4e2514665aa92df594f
Madhukar Sriramu <msriramu@akamai.com>
2020-02-24 13:01:53 -0500

    buggy implementation

--- exercism/c/c/word-count/src/word_count.c
+++ exercism/c/c/word-count/src/word_count.c
@@ -1,26 +1,48 @@
 #include "word_count.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 // returns the count of words in the string
 int word_count(const char *text, word_count_word_t *words) {
 	
 	char *word;
 	int wc = 0;
-	int index = 0;
 
-	word_count_word_t *words = malloc(sizeof(word_count_word_t) * MAX_WORDS);
+	words = malloc(sizeof(word_count_word_t) * MAX_WORDS);
 	memset(words, 0, sizeof(word_count_word_t) * MAX_WORDS);
 
-	while ((word = strtok(text, " ")) != NULL) {
-		int len = strlen(word);
+	while ((word = strtok((char *)text, " ")) != NULL) {
+		size_t len = strlen(word);
 		if (len > MAX_WORD_LENGTH) {
 			return EXCESSIVE_LENGTH_WORD;
 		}	
 		wc++;
 		
 		// search in words to find the string
-		words[i]					
+		word_count_word_t *tmp = words;
+		while (true) {
+			printf("%d \n", tmp->count);
+			// if not found
+			if (tmp->count == 0) {
+				memcpy(tmp->text, word, len);
+				tmp->count = 1;
+				break;
+			}
+
+			// if already exists
+			if (strncmp(tmp->text, word, MAX_WORD_LENGTH)) {
+				if (len == strlen(tmp->text)) {
+					tmp->count++;
+					break;
+				}
+			}
+
+			// not found, yet!
+			tmp++;
+		}
+		wc++;
 	}
 
 	if (wc > MAX_WORDS) {
