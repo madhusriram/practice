4ce6702982d9f2e9b0fe11f3aad62eca4ba1829d
Madhukar Sriramu <madhusirmv@gmail.com>
2020-03-08 19:23:28 -0400

    better scrabble implementation

--- exercism/go/go/scrabble-score/scrabble_score.go
+++ exercism/go/go/scrabble-score/scrabble_score.go
@@ -1,33 +1,32 @@
 // Package scrabble implements a scoring system
 package scrabble
 
-import (
-	"strings"
-	"unicode"
-)
-
-var m = map[string]int{
-	"aeioulnrst": 1,
-	"dg":         2,
-	"bcmp":       3,
-	"fhvwy":      4,
-	"k":          5,
-	"jx":         8,
-	"qz":         10,
-}
+import "unicode"
 
 // Score calculates the score of a scrabble word
 func Score(word string) int {
-	r := []rune(word)
 	var score int
 
-	for _, v := range r {
-		for k, s := range m {
-			if strings.ContainsRune(k, unicode.ToLower(v)) {
-				score += s
-				break
-			}
+	for _, v := range word {
+		v = unicode.ToLower(v)
+		switch v {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			score += 1
+		case 'd', 'g':
+			score += 2
+		case 'b', 'c', 'm', 'p':
+			score += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			score += 4
+		case 'k':
+			score += 5
+		case 'j', 'x':
+			score += 8
+		case 'q', 'z':
+			score += 10
+
 		}
 	}
+
 	return score
 }
