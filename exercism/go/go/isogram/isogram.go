2f2d154d578e7f8a5e138b0b5eb16250dc0b2e63
Madhukar Sriramu <madhusirmv@gmail.com>
2020-03-09 21:56:40 -0400

    isogram in golang

---
+++ exercism/go/go/isogram/isogram.go
@@ -0,0 +1,27 @@
+// Package isogram finds out if a string is a isogram
+package isogram
+
+import (
+	"unicode"
+)
+
+// IsIsogram returns true if a string is a isogram
+// else false
+func IsIsogram(s string) bool {
+	var mask uint32
+
+	for _, c := range s {
+		if ok := unicode.IsLetter(c); ok {
+			l := unicode.ToLower(c)
+			bit := uint32(1 << uint32(int(l)-97))
+
+			// check if bit is already set, if not set it
+			if mask&bit == bit {
+				return false
+			} else {
+				mask |= bit
+			}
+		}
+	}
+	return true
+}
