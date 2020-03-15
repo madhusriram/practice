a903858820c7d3ef36adbee14677b64969deca65
Madhukar Sriramu <madhusirmv@gmail.com>
2020-03-15 17:42:16 -0400

    acronym in C

---
+++ exercism/c/c/acronym/src/acronym.c
@@ -0,0 +1,34 @@
+#include "acronym.h"
+
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+char *abbreviate(const char *phrase) {
+	if (! phrase || strcmp(phrase, "") == 0)
+		return NULL;
+	
+	int len = strlen(phrase);
+	bool readWord = false;
+	char *res = (char *)malloc(len * sizeof(char));
+	int j = 0;
+
+	// iterate the array
+	for (int i = 0; i < len; i++) {
+		if (!readWord && isalpha(phrase[i])) {
+			readWord = true;
+			// add to the result array - pending
+			if (! isupper(phrase[i]))
+				res[j] = phrase[i] - 32;
+			else
+				res[j] = phrase[i];
+			j++;
+		}
+		if (phrase[i] == '-' || isspace(phrase[i]))
+			readWord = false;
+	}
+	res[j] = '\0';
+
+	return res;
+}
