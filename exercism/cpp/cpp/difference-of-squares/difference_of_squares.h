d7b0d3a0c74f6a9bcff3bdecf6d36b9ae99a9bef
Madhukar Sriramu <msriramu@akamai.com>
2020-03-16 16:53:05 -0400

    difference of squares

---
+++ exercism/cpp/cpp/difference-of-squares/difference_of_squares.h
@@ -0,0 +1,10 @@
+#if !defined(DIFFERENCE_OF_SQUARES_H)
+#define DIFFERENCE_OF_SQUARES_H
+
+namespace difference_of_squares {
+	unsigned square_of_sum(unsigned num);
+	unsigned sum_of_squares(unsigned num);
+	unsigned difference(unsigned num);
+}  // namespace difference_of_squares
+
+#endif // DIFFERENCE_OF_SQUARES_H
--- exercism/cpp/cpp/leap/leap.cpp
+++ exercism/cpp/cpp/leap/leap.cpp
@@ -2,7 +2,7 @@
 
 namespace leap {
 	bool is_leap_year(int year) {
-		if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 			return true;
 		return false;
 	}
