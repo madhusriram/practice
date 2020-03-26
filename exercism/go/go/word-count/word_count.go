0783d58631b48d557bfd70c95fe65bc284493922
Madhukar Sriramu <madhusirmv@gmail.com>
2020-03-25 20:49:42 -0400

    remove logic to remove quotations

--- exercism/go/go/word-count/word_count.go
+++ exercism/go/go/word-count/word_count.go
@@ -2,7 +2,6 @@ package wordcount
 
 import "strings"
 import "unicode"
-//import "fmt"
 
 // Frequency maps words to their occurrences
 type Frequency map[string]int
@@ -13,32 +12,16 @@ func WordCount(input string) Frequency {
 	var f Frequency = make(Frequency)
 
 	var s strings.Builder
-	var apostropheFound bool = false
 
 	for _, c := range input {
 		if unicode.IsLetter(c) || unicode.IsNumber(c) || c == '\'' {
-			// start of the word can only be a number or a letter
-			if s.Len() == 0 && c == '\'' {
-				continue
-			}
-			// if we got an apostrophe then it might be at the end, make sure
-			// we don't include that in the word
-			if c == '\'' {
-				apostropheFound = true
-				continue
-			}
-			// if an apostrophe was read in the previous read then add it now
-			// along with the current character
-			if apostropheFound {
-				s.WriteRune('\'')
-				apostropheFound = false
-			}
 			s.WriteRune(unicode.ToLower(c))
 		} else {
 			// get string and store in map
 			if s.Len() > 0 {
-				f[s.String()] += 1
-				apostropheFound = false
+				// trim all leading and trailing '
+				var res string = strings.Trim(s.String(), string('\''))
+				f[res]++
 				s.Reset()
 			}
 		}
@@ -46,7 +29,8 @@ func WordCount(input string) Frequency {
 
 	// last word
 	if s.Len() > 0 {
-		f[s.String()] += 1
+		var res string = strings.Trim(s.String(), string('\''))
+		f[res] += 1
 	}
 
 	return f
