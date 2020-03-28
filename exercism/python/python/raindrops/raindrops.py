f143a4c7f83f6ddc6e2348a2ead1787b3b5e13a0
Madhukar Sriramu <madhusirmv@gmail.com>
2020-03-28 10:42:03 -0400

    raindrops

---
+++ exercism/python/python/raindrops/raindrops.py
@@ -0,0 +1,11 @@
+def convert(number: int) -> str:
+    res = ""
+
+    if number % 3 == 0:
+        res += "Pling"
+    if number % 5 == 0:
+        res += "Plang"
+    if number % 7 == 0:
+        res += "Plong"
+
+    return res or str(number)
