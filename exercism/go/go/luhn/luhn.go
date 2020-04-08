d1bfa553dad5b76c7e5738e4f169bc90e1becde3
Madhukar Sriramu <madhusirmv@gmail.com>
2020-04-07 23:10:29 -0400

    completed and tested luhn

--- exercism/go/go/luhn/luhn.go
+++ exercism/go/go/luhn/luhn.go
@@ -5,6 +5,7 @@ import (
 	"unicode"
 )
 
+// Reverse reverse a string
 func Reverse(cc string) string {
     runes := []rune(cc)
     for i, j := 0, len(runes)-1; i < j; {
@@ -19,6 +20,8 @@ func Valid(cc string) bool {
 	l := len(cc)
 	// double lets us know when to double 
 	var double bool = false
+	var digits int
+
 	// single digit strings not valid
 	if l == 1 {
 		return false
@@ -28,26 +31,36 @@ func Valid(cc string) bool {
 	cc = Reverse(cc)
 
 	for _, d := range cc {
-		if unicode.IsNumber(d) {
-			n := int(d - '0')
-			if double {
-				num += (n * 2) % 9
-				double = false
-			} else {
-				num += n
-				double = true
-			}
-		} else if unicode.IsSpace(d) {
+		switch {
+			case unicode.IsNumber(d):
+				digits++
+				n := int(d - '0')
+				if double {
+					temp := n * 2
+					if temp > 9 {
+						temp -= 9
+					}
+					num += temp
+					double = false
+				} else {
+					num += n
+					double = true
+				}
+		case unicode.IsSpace(d):
 			continue
-		} else {
+		default:
 			return false
 		}
 	}
 
-	// if the number is evenly divisibly by 10 it's a valid credit card
-	if num % 10 != 0 || num == 0 {
-		return false
+	// case 1: single 0 is invalid
+	// case 2: number not divisible by 10 not valid
+	// this won't be caught in the length check earlier if the string has a
+	// space and a 0 like " 0"
+	if (num == 0 && digits == 1) || (num % 10 != 0) {
+		return false	
 	}
+
 	return true
 }
 
