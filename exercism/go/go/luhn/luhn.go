28ba1dd44aab1b49e9d7e4bc0361dbc923576d71
Madhukar Sriramu <madhusirmv@gmail.com>
2020-04-15 23:10:02 -0400

    luhn better implementation

--- exercism/go/go/luhn/luhn.go
+++ exercism/go/go/luhn/luhn.go
@@ -6,66 +6,47 @@ import (
 	"strings"
 )
 
-// RemoveSpaces removes spaces from a string
-func RemoveSpaces(str string) string {
-    var b strings.Builder
-    b.Grow(len(str))
-    for _, ch := range str {
-        if !unicode.IsSpace(ch) {
-            b.WriteRune(ch)
-        }
-    }
-    return b.String()
-}
-
 // Valid returns true if a credit card number is valid
 func Valid(cc string) bool {
 	var digits int
 
-	spacelessCc := RemoveSpaces(cc)
-	l := len(spacelessCc)
+	newCc := strings.ReplaceAll(cc, " ","")
+	lengthCc := len(newCc)
 	
 	// single digit strings not valid
-	if l == 1 {
+	if lengthCc == 1 {
 		return false
-	}
-
-	// based on the digit count we start at the
-	// first or second digit from the left to
-	// do the multiply by 2 logic
-	var double bool = false
-	if l%2 == 0 {
-		double = true
 	}
 
 	// accrue sum
 	var num int = 0
 
-	for _, d := range spacelessCc {
-		switch {
-		case unicode.IsNumber(d):
-			digits++
-			n := int(d - '0')
-			if double {
-				temp := n * 2
-				if temp > 9 {
-					temp -= 9
-				}
-				num += temp
-				double = false
-			} else {
-				num += n
-				double = true
-			}
-		default:
+	// start reading card from the left to right
+	for i := lengthCc; i > 0; i-- {
+		d := newCc[i-1]
+
+		if !unicode.IsNumber(rune(d)) {
 			return false
 		}
-	}
 
+		digits++
+		n := int(d - '0')
+	
+		// odd positions go through the doubling and subtracting by 9 logic
+		if (lengthCc-i)%2 != 0 {
+			n = n * 2
+			if n > 9 {
+				n = n%10 + n/10
+			}
+		}
+
+		num += n
+	}
+	
 	// case 1: single 0 is invalid
 	// case 2: number not divisible by 10 not valid
 	// this won't be caught in the length check earlier if the string has a
-	// space and a 0 like " 0"
+	// space and a 0 like " 0" hence the digits count kept earlier will be helpful
 	if (num == 0 && digits == 1) || (num%10 != 0) {
 		return false
 	}
