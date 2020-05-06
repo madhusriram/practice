cf4f2d94a81e5da8d18c85d4361843d8cdd953a8
Madhukar Sriramu <madhusirmv@gmail.com>
2020-05-05 23:07:56 -0400

    sieve of erathosthenes

---
+++ exercism/go/go/sieve/sieve.go
@@ -0,0 +1,40 @@
+// Package sieve calculates prime numbers until limit using Sieve of Erathosthenes algorithm
+package sieve
+
+import (
+)
+
+// Sieve returns all primes between 2 to limit
+func Sieve(limit int) []int {
+	primes := []int{}
+
+	// no primes under 2
+	if limit < 2 {
+		return primes
+	}
+
+	// list contains numbers from 2 to limit
+	numberList := make([]int, limit-1)
+
+	// iterate through numberList and add to primes
+	// if that location is 0
+	for i, v := range numberList {
+		if v == 0 {
+			primes = append(primes, i+2)
+		}
+
+		number := i + 2
+
+		// set all multiples of number to 1
+		for x := number; x <= limit; x += number {
+			numberList[x-2] = 1
+		}
+	}
+
+	return primes
+}
+
+/*
+0 1 2 3 4 
+2 3 4 5 6
+*/
